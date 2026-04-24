9a05fc738320fbd35277a0b37f651fef3d534f99
Blank <godwinjay735@gmail.com>
2026-04-24 11:43:37 +0800

    Add login form UI and authentication logic
    Added UI controls for login form, including username, password, login, and close buttons. Implemented event handlers for login and close actions. Displays appropriate messages and opens corresponding forms on successful login.

--- Clinick/Forms/LoginFrm.Designer.vb
+++ Clinick/Forms/LoginFrm.Designer.vb
@@ -22,17 +22,92 @@ Partial Class LoginFrm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        btnLogin = New Button()
+        txtUsername = New TextBox()
+        txtPassword = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        btnClose = New Label()
         SuspendLayout()
+        ' 
+        ' btnLogin
+        ' 
+        btnLogin.Location = New Point(109, 371)
+        btnLogin.Name = "btnLogin"
+        btnLogin.Size = New Size(110, 40)
+        btnLogin.TabIndex = 0
+        btnLogin.Text = "LOGIN"
+        btnLogin.UseVisualStyleBackColor = True
+        ' 
+        ' txtUsername
+        ' 
+        txtUsername.BorderStyle = BorderStyle.FixedSingle
+        txtUsername.Location = New Point(84, 222)
+        txtUsername.Name = "txtUsername"
+        txtUsername.Size = New Size(170, 23)
+        txtUsername.TabIndex = 1
+        ' 
+        ' txtPassword
+        ' 
+        txtPassword.BorderStyle = BorderStyle.FixedSingle
+        txtPassword.Location = New Point(84, 296)
+        txtPassword.Name = "txtPassword"
+        txtPassword.Size = New Size(170, 23)
+        txtPassword.TabIndex = 2
+        txtPassword.UseSystemPasswordChar = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(84, 278)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(60, 15)
+        Label1.TabIndex = 3
+        Label1.Text = "Password:"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(84, 204)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(63, 15)
+        Label2.TabIndex = 4
+        Label2.Text = "Username:"
+        ' 
+        ' btnClose
+        ' 
+        btnClose.AutoSize = True
+        btnClose.Font = New Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnClose.Location = New Point(321, 9)
+        btnClose.Name = "btnClose"
+        btnClose.Size = New Size(24, 25)
+        btnClose.TabIndex = 5
+        btnClose.Text = "X"
         ' 
         ' LoginFrm
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(357, 626)
+        Controls.Add(btnClose)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(txtPassword)
+        Controls.Add(txtUsername)
+        Controls.Add(btnLogin)
+        FormBorderStyle = FormBorderStyle.None
         Name = "LoginFrm"
         StartPosition = FormStartPosition.CenterScreen
         Text = "Form1"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnClose As Label
 
 End Class
--- Clinick/Forms/LoginFrm.vb
+++ Clinick/Forms/LoginFrm.vb
@@ -1,3 +1,30 @@
 ﻿Public Class LoginFrm
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Close()
+    End Sub
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        Dim username As String = txtUsername.Text
+        Dim password As String = txtPassword.Text
+        Dim employee As String = "Employee"
+        Dim employeePass As String = "Clinick123"
+        Dim admin As String = "Admin"
+        Dim adminPass As String = "Admin123"
+
+        If username = employee AndAlso password = employeePass Then
+            MessageBox.Show("Login successful! Welcome, Employee.")
+            Dim employeeFrm As New MainFrm()
+            employeeFrm.Show()
+            Me.Hide()
+        ElseIf username = admin AndAlso password = adminPass Then
+            MessageBox.Show("Login successful! Welcome, Admin.")
+            Dim adminFrm As New AdminFrm()
+            adminFrm.Show()
+            Me.Hide()
+        Else
+            MessageBox.Show("Invalid username or password. Please try again.")
+        End If
+
+    End Sub
 
 End Class

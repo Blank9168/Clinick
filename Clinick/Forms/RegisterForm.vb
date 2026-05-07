5eed1112f4b83798ffc2512f46838664a282cff3
hojungie <jenjen.tomida@gmail.com>
2026-05-07 21:21:42 +0800

    register formmm

--- Clinick/Forms/RegisterForm.vb
+++ Clinick/Forms/RegisterForm.vb
@@ -165,4 +165,11 @@
         RefreshAvailableSlots()
     End Sub
 
+    Private Sub Label10_Click(sender As Object, e As EventArgs) Handles Label10.Click
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
 End Class

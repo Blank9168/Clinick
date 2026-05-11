a244915050d4ae2e33457f694c9d0a64aab0e041
JDomii <jiandoms@gmail.com>
2026-05-11 20:24:57 +0800

    Designed the UI in Patient Records

--- Clinick/My Project/Resources.Designer.vb
+++ Clinick/My Project/Resources.Designer.vb
@@ -223,6 +223,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property Iconsss__9_() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("Iconsss (9)", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property Ipin() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("Ipin", resourceCulture)
--- Clinick/PatientRecords_Frm.Designer.vb
+++ Clinick/PatientRecords_Frm.Designer.vb
@@ -22,6 +22,7 @@ Partial Class PatientRecords_Frm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(PatientRecords_Frm))
         txtSearchName = New MaskedTextBox()
         btnSearch = New Button()
         dgvPatientHistory = New DataGridView()
@@ -32,38 +33,55 @@ Partial Class PatientRecords_Frm
         Sex = New DataGridViewTextBoxColumn()
         DOB = New DataGridViewTextBoxColumn()
         btnClose = New Button()
+        PictureBox1 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        Label2 = New Label()
+        PictureBox3 = New PictureBox()
+        PictureBox6 = New PictureBox()
+        PictureBox4 = New PictureBox()
         CType(dgvPatientHistory, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox6, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox4, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' txtSearchName
         ' 
-        txtSearchName.Location = New Point(168, 70)
+        txtSearchName.BorderStyle = BorderStyle.FixedSingle
+        txtSearchName.Location = New Point(53, 124)
         txtSearchName.Name = "txtSearchName"
         txtSearchName.Size = New Size(315, 23)
         txtSearchName.TabIndex = 0
         ' 
         ' btnSearch
         ' 
-        btnSearch.Location = New Point(566, 63)
+        btnSearch.BackColor = Color.Teal
+        btnSearch.FlatStyle = FlatStyle.Flat
+        btnSearch.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnSearch.ForeColor = Color.Transparent
+        btnSearch.Location = New Point(383, 117)
         btnSearch.Name = "btnSearch"
-        btnSearch.Size = New Size(120, 35)
+        btnSearch.Size = New Size(112, 35)
         btnSearch.TabIndex = 1
-        btnSearch.Text = "search"
-        btnSearch.UseVisualStyleBackColor = True
+        btnSearch.Text = "    Search"
+        btnSearch.UseVisualStyleBackColor = False
         ' 
         ' dgvPatientHistory
         ' 
         dgvPatientHistory.AllowUserToAddRows = False
         dgvPatientHistory.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+        dgvPatientHistory.BackgroundColor = Color.FromArgb(CByte(221), CByte(240), CByte(243))
         dgvPatientHistory.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing
         dgvPatientHistory.Columns.AddRange(New DataGridViewColumn() {ID, Patient, Contact, Age, Sex, DOB})
-        dgvPatientHistory.Location = New Point(102, 161)
+        dgvPatientHistory.Location = New Point(28, 184)
         dgvPatientHistory.MultiSelect = False
         dgvPatientHistory.Name = "dgvPatientHistory"
         dgvPatientHistory.ReadOnly = True
         dgvPatientHistory.RowHeadersVisible = False
         dgvPatientHistory.SelectionMode = DataGridViewSelectionMode.FullRowSelect
-        dgvPatientHistory.Size = New Size(648, 376)
+        dgvPatientHistory.Size = New Size(828, 376)
         dgvPatientHistory.TabIndex = 2
         ' 
         ' ID
@@ -104,27 +122,103 @@ Partial Class PatientRecords_Frm
         ' 
         ' btnClose
         ' 
-        btnClose.Location = New Point(746, 49)
+        btnClose.FlatStyle = FlatStyle.Flat
+        btnClose.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnClose.ForeColor = Color.Teal
+        btnClose.Location = New Point(738, 116)
         btnClose.Name = "btnClose"
-        btnClose.Size = New Size(53, 34)
+        btnClose.Size = New Size(95, 34)
         btnClose.TabIndex = 3
-        btnClose.Text = "close"
+        btnClose.Text = "     Close"
         btnClose.UseVisualStyleBackColor = True
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackgroundImage = My.Resources.Resources.Reports_Banner__2_
+        PictureBox1.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox1.Location = New Point(-3, -4)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(961, 87)
+        PictureBox1.TabIndex = 11
+        PictureBox1.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.BackColor = Color.FromArgb(CByte(249), CByte(251), CByte(253))
+        PictureBox2.Location = New Point(28, 99)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(828, 70)
+        PictureBox2.TabIndex = 12
+        PictureBox2.TabStop = False
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.ForeColor = Color.Teal
+        Label2.Location = New Point(79, 16)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(192, 32)
+        Label2.TabIndex = 14
+        Label2.Text = "Patient Records"
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.BackgroundImage = My.Resources.Resources.Iconsss__9_
+        PictureBox3.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox3.Location = New Point(12, 15)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(58, 50)
+        PictureBox3.TabIndex = 15
+        PictureBox3.TabStop = False
+        ' 
+        ' PictureBox6
+        ' 
+        PictureBox6.BackgroundImage = CType(resources.GetObject("PictureBox6.BackgroundImage"), Image)
+        PictureBox6.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox6.Location = New Point(749, 121)
+        PictureBox6.Name = "PictureBox6"
+        PictureBox6.Size = New Size(14, 25)
+        PictureBox6.TabIndex = 16
+        PictureBox6.TabStop = False
+        ' 
+        ' PictureBox4
+        ' 
+        PictureBox4.BackColor = Color.Teal
+        PictureBox4.BackgroundImage = CType(resources.GetObject("PictureBox4.BackgroundImage"), Image)
+        PictureBox4.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox4.Location = New Point(394, 122)
+        PictureBox4.Name = "PictureBox4"
+        PictureBox4.Size = New Size(21, 25)
+        PictureBox4.TabIndex = 17
+        PictureBox4.TabStop = False
         ' 
         ' PatientRecords_Frm
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(249), CByte(251), CByte(253))
         ClientSize = New Size(880, 595)
+        Controls.Add(PictureBox4)
+        Controls.Add(PictureBox6)
+        Controls.Add(PictureBox3)
+        Controls.Add(Label2)
         Controls.Add(btnClose)
         Controls.Add(dgvPatientHistory)
         Controls.Add(btnSearch)
         Controls.Add(txtSearchName)
+        Controls.Add(PictureBox1)
+        Controls.Add(PictureBox2)
         FormBorderStyle = FormBorderStyle.None
         Name = "PatientRecords_Frm"
         StartPosition = FormStartPosition.CenterScreen
         Text = "PatientRecords_Frm"
         CType(dgvPatientHistory, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox6, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox4, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
         PerformLayout()
     End Sub
@@ -139,4 +233,10 @@ Partial Class PatientRecords_Frm
     Friend WithEvents Age As DataGridViewTextBoxColumn
     Friend WithEvents Sex As DataGridViewTextBoxColumn
     Friend WithEvents DOB As DataGridViewTextBoxColumn
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
 End Class
